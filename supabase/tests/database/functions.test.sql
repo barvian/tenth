34b990beaf091540ad55d720a30fe180ada0a31e
Maxwell Barvian <max@barvian.me>
2022-11-20 13:59:45 -0600

    Remove seed to hopefully help random signup errors

--- supabase/tests/database/functions.test.sql
+++ supabase/tests/database/functions.test.sql
@@ -1,23 +1,29 @@
 begin;
 select plan(3);
 
-select
-    ok(
-        email_exists('max@barvian.me') = false,
-        'email_exists() fails for new users'
-    );
+insert into auth.users (id, email) values
+('b9511b07-87eb-4e02-bfb5-3b7095129c73', 'unregistered@test.com'),
+('6ce7faef-966c-40f3-bb32-d7852c6e8430', 'registered@test.com');
 
-select
-    ok(
-        email_exists('unregistered@test.com') = false,
-        'email_exists() fails for unregistered users'
-    );
+insert into public.profiles (user_id, first_name, last_name, stripe_id, change_id, percentage) values
+('6ce7faef-966c-40f3-bb32-d7852c6e8430', 'Registered', 'User', 'fake-stripe-id', 'fake-change-id', 0.100);
 
-select
-    ok(
-        email_exists('registered@test.com'),
-        'email_exists() passes for registered users'
-    );
+-- 
+
+select ok(
+    email_exists('max@barvian.me') = false,
+    'email_exists() fails for new users'
+);
+
+select ok(
+    email_exists('unregistered@test.com') = false,
+    'email_exists() fails for unregistered users'
+);
+
+select ok(
+    email_exists('registered@test.com'),
+    'email_exists() passes for registered users'
+);
 
 select * from finish();
 rollback;
--- supabase/tests/database/profiles.test.sql
+++ supabase/tests/database/profiles.test.sql
@@ -1,76 +1,78 @@
 begin;
 select plan(7);
 
+insert into auth.users (id, email) values
+('b9511b07-87eb-4e02-bfb5-3b7095129c73', 'unregistered@test.com'),
+('6ce7faef-966c-40f3-bb32-d7852c6e8430', 'registered@test.com');
+
+insert into public.profiles (user_id, first_name, last_name, stripe_id, change_id, percentage) values
+('6ce7faef-966c-40f3-bb32-d7852c6e8430', 'Registered', 'User', 'fake-stripe-id', 'fake-change-id', 0.100);
+
+-- 
+
 set local role authenticated;
-set local "request.jwt.claim.sub" to 'b9511b07-87eb-4e02-bfb5-3b7095129c73'; -- unregistered user
+set local "request.jwt.claim.sub" to 'b9511b07-87eb-4e02-bfb5-3b7095129c73';
 
-select
-    throws_ok(
-        $$
-        insert into public.profiles (user_id, stripe_id, change_id) values
-        ('b9511b07-87eb-4e02-bfb5-3b7095129c73', 'fake-stripe-id', 'fake-change-id');
-        $$,
-        42501,
-        'new row violates row-level security policy for table "profiles"',
-        'A user cannot insert a profile outside register()'
+select throws_ok(
+    $$
+    insert into public.profiles (user_id, stripe_id, change_id) values
+    ('b9511b07-87eb-4e02-bfb5-3b7095129c73', 'fake-stripe-id', 'fake-change-id');
+    $$,
+    42501,
+    'new row violates row-level security policy for table "profiles"',
+    'A user cannot insert a profile outside register()'
+);
+
+select lives_ok(
+    $$
+    select register(
+        'fake-stripe-id2',
+        'fake-change-id2',
+        'Registered2', 'User',
+        0.100,
+        '{"fake-charity-id"}'
     );
+    $$,
+    'register() succeeds for unregistered users'
+);
 
-select
-    lives_ok(
-        $$
-        select register(
-            'fake-stripe-id2',
-            'fake-change-id2',
-            'Registered2', 'User',
-            0.100,
-            '{"fake-charity-id"}'
-        );
-        $$,
-        'register() succeeds for unregistered users'
-    );
+select results_eq(
+    $$
+    select first_name from public.profiles;
+    $$,
+    $$
+    values('Registered2')
+    $$,
+    'A user can only read their own profile'
+);
 
-select
-    results_eq(
-        $$
-        select first_name from public.profiles;
-        $$,
-        $$
-        values('Registered2')
-        $$,
-        'A user can only read their own profile'
-    );
+select is_empty(
+    $$
+    update public.profiles set last_name = 'Last' where first_name = 'Registered' returning *;
+    $$,
+    'A user cannot update another profile'
+);
 
-select
-    is_empty(
-        $$
-        update public.profiles set last_name = 'Last' where first_name = 'Registered' returning *;
-        $$,
-        'A user cannot update another profile'
-    );
-
-select
-  is_empty(
+select is_empty(
     $$
     delete from public.profiles returning *;
     $$,
     'A user cannot delete any profiles'
-  );
+);
 
-select
-    is_empty(
-        $$
-        update public.profiles set stripe_id = 'fake-stripe-id' returning *;
-        $$,
-        'A user cannot set their stripe_id'
-    );
+select is_empty(
+    $$
+    update public.profiles set stripe_id = 'fake-stripe-id' returning *;
+    $$,
+    'A user cannot set their stripe_id'
+);
 
-select
-    is_empty(
-        $$
-        update public.profiles set change_id = 'fake-change-id' returning *;
-        $$,
-        'A user cannot set their change_id'
-    );
+select is_empty(
+    $$
+    update public.profiles set change_id = 'fake-change-id' returning *;
+    $$,
+    'A user cannot set their change_id'
+);
 
 
 select * from finish();
