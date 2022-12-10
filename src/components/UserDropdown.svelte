cd0cba8cb5587816980d6b97033d967c46043eb7
Maxwell Barvian <max@barvian.me>
2022-12-10 10:44:28 -0600

    Email in user dropdown

--- src/components/UserDropdown.svelte
+++ src/components/UserDropdown.svelte
@@ -9,8 +9,12 @@
     let open = false
 
     $: active = $page.url.pathname === '/account'
-    $: email = $page.data.session!.user.email!
-    $: [username, domain] = email.split('@')
+    let email: string
+    $: {
+        email = $page.data.session!.user.email!
+        const [username, ...emailParts] = email.split('@')
+        email = [username.length > 9 ? username.charAt(0)+'..'+username.slice(-1) : username, ...emailParts].join('@')
+    }
 
     afterNavigate(() => open = false)
 
@@ -24,10 +28,13 @@
 <details class="group relative inline-block" bind:open use:clickOutside on:outclick={() => open = false}>
     <summary class="cursor-pointer whitespace-nowrap select-none group-open:text-black {active ? '!text-black' : 'text-gray-500'}" aria-haspopup="menu">
         <img src="https://www.gravatar.com/avatar/{md5(email.toLowerCase().trim())}?d=mp" alt="Your avatar" class="rounded-full aspect-square object-fill mr-1.5 h-6 align-middle inline-block group-open:!opacity-100" class:opacity-60={!active} />
-        <span class="hidden lg:inline">{username.length > 9 ? username.charAt(0) + '..' + username.slice(-1) : username}@{domain}</span>
+        <span class="hidden lg:inline">{email}</span>
         <Caret strokeWidth={1} class="inline-block align-middle md:ml-1 h-1.5" />
     </summary>
     <div class="rounded-2xl text-left border p-2 z-50 bg-white right-0 absolute top-full mt-4 min-w-[200px] shadow animate-fly-t" role="menu">
+        <span class="block lg:hidden border-b border-gray-200 p-3 mb-2 text-gray-500">
+            {email}
+        </span>
         <a href="/account" role="menuitem" class="block hover:bg-gray-100 p-3 rounded-xl w-full">Edit account</a>
         <form action="/logout" method="POST" use:enhance>
             <button type="submit" class="block hover:bg-gray-100 p-3 w-full text-left rounded-xl">Log out</button>
