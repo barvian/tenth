85096611eebb7f6cc9d055608f9710d6ee196115
Maxwell Barvian <max@barvian.me>
2022-12-16 12:21:28 -0600

    Remove more non-MVP stuff

--- src/lib/promises.ts
+++ src/lib/promises.ts
@@ -1,11 +1,3 @@
 import { browser } from "$app/environment"
 
-// Returns a promise with the page loaded status
-export const pageLoaded: Promise<boolean> = !browser ? Promise.resolve(false) : new Promise(resolve => {
-	if (document.readyState === 'complete') return resolve(true)
-	window.addEventListener('load', event => {
-		resolve(true)
-	})
-})
-
 export const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
--- src/routes/(posts)/about/+page.md
+++
@@ -1,5 +0,0 @@
----
-title: About
----
-
-Tenth started as a humble side project by [Maxwell Barvian](https://barvian.me), a web developer based outside of Chicago, IL.
