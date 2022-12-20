b5a19840778223f8e752bfa1ff5e735ce181049a
Maxwell Barvian <max@barvian.me>
2022-12-20 11:44:58 -0600

    Success handling in profile update

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <h1 class="text-4xl font-bold text-center mb-8 max-w-2xl">Your profile</h1>
-<Form action="?/update" class="max-w-sm w-full" let:complete>
+<div class="max-w-sm w-full">
 	<Input
 		class="mb-3"
 		label="First name"
@@ -23,31 +23,40 @@
 		disabled
 		description="You're not able to change your name at this time."
 	/>
-	<Input
-		class="mt-6"
-		type="email"
-		label="Email"
-		name="email"
-		bind:value={email}
-		disabled={complete}
-		showDescription={!complete && email !== data.session?.user.email}
-		description="We'll send you a confirmation to complete your email change."
-	/>
-	<Button class="mt-8" type="submit" disabled={complete}>Save profile</Button>
-</Form>
-<Form
-	action="?/delete"
-	class="max-w-sm w-full"
-	on:submit={(event) => {
-		if (!confirm('Are you sure you want to delete your account?'))
-			event.preventDefault()
-	}}
->
-	<Button
-		unstyled
-		class="text-red-400 font-medium text-lg mt-8 w-full text-center"
-		type="submit"
+	<Form action="?/update" let:complete>
+		<p
+			slot="complete"
+			class="mt-6 bg-gray-100 rounded-3xl p-6 text-center"
+			role="alert"
+		>
+			Click the link we sent to your new and old email addresses to confirm your
+			change.
+		</p>
+		<Input
+			class="mt-6"
+			type="email"
+			label="Email"
+			name="email"
+			bind:value={email}
+			disabled={complete}
+			showDescription={!complete && email !== data.session?.user.email}
+			description="We'll send you a confirmation to complete your email change."
+		/>
+		<Button class="mt-8" type="submit" disabled={complete}>Save profile</Button>
+	</Form>
+	<Form
+		action="?/delete"
+		on:submit={(event) => {
+			if (!confirm('Are you sure you want to delete your account?'))
+				event.preventDefault()
+		}}
 	>
-		Delete account
-	</Button>
-</Form>
+		<Button
+			unstyled
+			class="text-red-400 font-medium text-lg mt-8 w-full text-center"
+			type="submit"
+		>
+			Delete account
+		</Button>
+	</Form>
+</div>
