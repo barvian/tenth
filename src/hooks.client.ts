7ce57a7cb8f7a6c4cf1d944df6c4797dfce0f8ae
Maxwell Barvian <max@barvian.me>
2023-01-03 13:57:21 -0600

    Sentry init

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,13 +1,23 @@
 import { dev } from '$app/environment'
-import type { HandleClientError } from '@sveltejs/kit'
-import '~/lib/db'
+import { page } from '$app/stores'
 // import '~/../mocks/browser';
+import * as Sentry from '@sentry/browser'
+import type { HandleClientError } from '@sveltejs/kit'
+import { get } from 'svelte/store'
+import '~/lib/db'
+
+Sentry.init({
+	dsn: 'https://8e0ebee7a3ef44a6af1745d490cac1bf@o4504442247380992.ingest.sentry.io/4504442252230657'
+})
 
 export const handleError: HandleClientError = ({ error, event }) => {
 	if (dev) {
 		console.error(error, event)
 	} else {
-		// Sentry.captureException(error, { event });
+		Sentry.captureException(error, {
+			user: get(page).data.session?.user,
+			extra: { event }
+		})
 	}
 
 	return {
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,24 @@
 import { dev } from '$app/environment'
+import * as Sentry from '@sentry/node'
+import { getSupabase } from '@supabase/auth-helpers-sveltekit'
 import type { HandleServerError } from '@sveltejs/kit'
 import '~/../mocks/server'
 import '~/lib/db'
+
+Sentry.init({
+	dsn: 'https://a86a93f598414db9ace55da039f9ed03@o4504442247380992.ingest.sentry.io/4504442255114240'
+})
 
 export const handleError: HandleServerError = ({ error, event }) => {
 	if (dev) {
 		console.error(error)
 	} else {
-		// Sentry.captureException(error, { event });
+		getSupabase(event).then(({ session }) => {
+			Sentry.captureException(error, {
+				user: session?.user,
+				extra: { event }
+			})
+		})
 	}
 
 	return {
