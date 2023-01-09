89f1964900fea6c964646201dd357728917d8a89
Maxwell Barvian <max@barvian.me>
2023-01-09 12:54:49 -0600

    Plaid production switch

--- src/lib/plaid.ts
+++ src/lib/plaid.ts
@@ -1,8 +1,9 @@
+import { dev } from '$app/environment'
 import { PLAID_CLIENT_ID, SECRET_PLAID_KEY } from '$env/static/private'
 import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid'
 
 const configuration = new Configuration({
-	basePath: PlaidEnvironments.sandbox,
+	basePath: dev ? PlaidEnvironments.sandbox : PlaidEnvironments.production,
 	baseOptions: {
 		headers: {
 			'PLAID-CLIENT-ID': PLAID_CLIENT_ID,
