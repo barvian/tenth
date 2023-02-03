be686c17321ddfc4d9c26a9d8bacceecb14b9386
Maxwell Barvian <max@barvian.me>
2023-02-03 01:30:08 -0600

    Lowercase SQL

--- supabase/migrations/20221118171950_init.sql
+++ supabase/migrations/20221118171950_init.sql
@@ -1,220 +1,220 @@
-CREATE EXTENSION citext;
-CREATE DOMAIN email AS citext
-  CHECK ( value ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$' );
+create extension citext;
+create domain email as citext
+  check ( value ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$' );
 
-CREATE TYPE account_status AS ENUM ('active', 'inactive', 'disconnected');
+create type account_status as enum ('active', 'inactive', 'disconnected');
 
-CREATE TABLE IF NOT EXISTS public.accounts
+create table if not exists public.accounts
 (
     id serial,
-    user_id uuid NOT NULL DEFAULT auth.uid(),
-    stripe_pm_id text UNIQUE,
-    stripe_fca_id text UNIQUE,
+    user_id uuid not null default auth.uid(),
+    stripe_pm_id text unique,
+    stripe_fca_id text unique,
     last4 text,
     institution_name text,
-    created_at timestamptz NOT NULL DEFAULT NOW(),
-    percentage numeric(3,3) DEFAULT 0.100 NOT NULL CHECK (percentage > 0),
-    status account_status NOT NULL DEFAULT 'active',
-    CONSTRAINT accounts_pkey PRIMARY KEY (id),
-    CONSTRAINT accounts_user_id_fkey FOREIGN KEY (user_id)
-        REFERENCES auth.users (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE
+    created_at timestamptz not null default now(),
+    percentage numeric(3,3) default 0.100 not null check (percentage > 0),
+    status account_status not null default 'active',
+    constraint accounts_pkey primary key (id),
+    constraint accounts_user_id_fkey foreign key (user_id)
+        references auth.users (id) match simple
+        on update no action
+        on delete cascade
 );
 
-ALTER TABLE IF EXISTS public.accounts
-    ENABLE ROW LEVEL SECURITY;
+alter table if exists public.accounts
+    enable row level security;
 
-CREATE POLICY "Owner can read their own"
-    ON public.accounts
-    AS PERMISSIVE
-    FOR SELECT
-    TO public
-    USING (auth.uid() = user_id);
+create policy "Owner can read their own"
+    on public.accounts
+    as permissive
+    for select
+    to public
+    using (auth.uid() = user_id);
 
-CREATE TABLE IF NOT EXISTS public.users
+create table if not exists public.users
 (
-    id uuid NOT NULL DEFAULT auth.uid(),
+    id uuid not null default auth.uid(),
     first_name text,
     last_name text,
-    stripe_cus_id text NOT NULL UNIQUE,
+    stripe_cus_id text not null unique,
     current_account_id int,
     last_acceptance jsonb,
-    CONSTRAINT users_pkey PRIMARY KEY (id),
-    CONSTRAINT users_user_id_fkey FOREIGN KEY (id)
-        REFERENCES auth.users (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE,
-    CONSTRAINT users_current_account_id_fkey FOREIGN KEY (current_account_id)
-        REFERENCES public.accounts (id) MATCH SIMPLE
+    constraint users_pkey primary key (id),
+    constraint users_user_id_fkey foreign key (id)
+        references auth.users (id) match simple
+        on update no action
+        on delete cascade,
+    constraint users_current_account_id_fkey foreign key (current_account_id)
+        references public.accounts (id) match simple
 );
 
-CREATE FUNCTION email(public.users) RETURNS text SECURITY DEFINER STABLE AS $$
-  SELECT email::text FROM auth.users a WHERE a.id = $1.id
-$$ LANGUAGE SQL;
+create function email(public.users) returns text security definer stable as $$
+  select email::text from auth.users a where a.id = $1.id
+$$ language sql;
 
-REVOKE UPDATE ON public.users FROM anon, authenticated;
-GRANT UPDATE ("first_name", "last_name") ON public.users TO authenticated;
+revoke update on public.users from anon, authenticated;
+grant update ("first_name", "last_name") on public.users to authenticated;
 
-ALTER TABLE IF EXISTS public.users
-    ENABLE ROW LEVEL SECURITY;
+alter table if exists public.users
+    enable row level security;
 
-CREATE POLICY "Owner can read own profile"
-    ON public.users
-    AS PERMISSIVE
-    FOR SELECT
-    TO public
-    USING (auth.uid() = id);
-CREATE POLICY "Owner can update own profile"
-    ON public.users
-    AS PERMISSIVE
-    FOR UPDATE
-    TO public
-    USING (auth.uid() = id);
+create policy "Owner can read own profile"
+    on public.users
+    as permissive
+    for select
+    to public
+    using (auth.uid() = id);
+create policy "Owner can update own profile"
+    on public.users
+    as permissive
+    for update
+    to public
+    using (auth.uid() = id);
 
-CREATE FUNCTION set_active_account()
-  RETURNS TRIGGER AS $$
-  BEGIN
-    UPDATE public.users SET current_account_id = NEW.id WHERE id = NEW.user_id;
-    RETURN NULL;
-  END;
-$$ LANGUAGE plpgsql;
+create function set_active_account()
+  returns trigger as $$
+  begin
+    update public.users set current_account_id = new.id where id = new.user_id;
+    return null;
+  end;
+$$ language plpgsql;
 
-CREATE FUNCTION maybe_attach_to_account()
-  RETURNS TRIGGER AS $$
-  DECLARE
+create function maybe_attach_to_account()
+  returns trigger as $$
+  declare
     unused_id int;
-  BEGIN
-    SELECT id INTO unused_id FROM public.accounts WHERE user_id = NEW.user_id AND stripe_fca_id IS NULL LIMIT 1;
+  begin
+    select id into unused_id from public.accounts where user_id = new.user_id and stripe_fca_id is null limit 1;
 
-    IF NEW.stripe_fca_id IS NOT NULL AND unused_id IS NOT NULL THEN
-        UPDATE public.accounts a
-        SET
-            stripe_pm_id = NEW.stripe_pm_id,
-            stripe_fca_id = NEW.stripe_fca_id,
-            last4 = NEW.last4,
-            institution_name = NEW.institution_name
-        WHERE id = unused_id;
+    if new.stripe_fca_id is not null and unused_id is not null then
+        update public.accounts a
+        set
+            stripe_pm_id = new.stripe_pm_id,
+            stripe_fca_id = new.stripe_fca_id,
+            last4 = new.last4,
+            institution_name = new.institution_name
+        where id = unused_id;
 
-        RETURN NULL;
-    END IF;
+        return null;
+    end if;
 
-    RETURN NEW;
-  END;
-$$ LANGUAGE plpgsql;
+    return new;
+  end;
+$$ language plpgsql;
 
-CREATE TRIGGER handle_account_before_insert
-  BEFORE INSERT ON public.accounts
-  FOR EACH ROW
-  EXECUTE PROCEDURE maybe_attach_to_account();
+create trigger handle_account_before_insert
+  before insert on public.accounts
+  for each row
+  execute procedure maybe_attach_to_account();
 
-CREATE TRIGGER handle_account_after_insert
-  AFTER INSERT ON public.accounts
-  FOR EACH ROW
-  EXECUTE PROCEDURE set_active_account();
+create trigger handle_account_after_insert
+  after insert on public.accounts
+  for each row
+  execute procedure set_active_account();
 
-CREATE TYPE designated_input as(
+create type designated_input as(
     pledge_org_id text,
     weight float
 );
 
-CREATE OR REPLACE FUNCTION public.register(
+create or replace function public.register(
 	stripe_cus_id text,
     first_name text,
     last_name text,
 	percentage numeric,
 	designated designated_input[]
 )
-    RETURNS void
-    SECURITY DEFINER
-    LANGUAGE 'plpgsql'
-    VOLATILE
-AS $$
-BEGIN
-    INSERT INTO public.users (stripe_cus_id, first_name, last_name) VALUES ($1, $2, $3);
-    WITH new_account AS (
-        INSERT INTO public.accounts (percentage) VALUES ($4) RETURNING *
+    returns void
+    security definer
+    language 'plpgsql'
+    volatile
+as $$
+begin
+    insert into public.users (stripe_cus_id, first_name, last_name) values ($1, $2, $3);
+    with new_account as (
+        insert into public.accounts (percentage) values ($4) returning *
     )
-    INSERT INTO public.designated (account_id, pledge_org_id, weight) SELECT new_account.id, d.pledge_org_id, d.weight FROM UNNEST($5) d, new_account;
-END;
+    insert into public.designated (account_id, pledge_org_id, weight) select new_account.id, d.pledge_org_id, d.weight from unnest($5) d, new_account;
+end;
 $$;
 
-CREATE OR REPLACE FUNCTION public.email_exists(
+create or replace function public.email_exists(
 	email character varying)
-    RETURNS boolean
-    SECURITY DEFINER
-    LANGUAGE 'plpgsql'
-    STABLE
-    SET search_path=auth, pg_temp
-AS $$
-BEGIN
-    RETURN EXISTS(
-        SELECT 1
-        FROM auth.users au
-        WHERE au.email = $1
-        AND EXISTS(
-            SELECT 1
-            FROM public.users pu
-            WHERE pu.id = au.id
+    returns boolean
+    security definer
+    language 'plpgsql'
+    stable
+    set search_path=auth, pg_temp
+as $$
+begin
+    return exists(
+        select 1
+        from auth.users au
+        where au.email = $1
+        and exists(
+            select 1
+            from public.users pu
+            where pu.id = au.id
         )
     );
-END;
+end;
 $$;
 
-CREATE TABLE IF NOT EXISTS public.designated
+create table if not exists public.designated
 (
-    user_id uuid DEFAULT auth.uid(),
+    user_id uuid default auth.uid(),
     account_id int,
     pledge_org_id text,
-    weight float NOT NULL DEFAULT 1 CHECK (weight > 0),
-    created_at timestamptz NOT NULL DEFAULT NOW(),
-    CONSTRAINT designated_pkey PRIMARY KEY (user_id, account_id, pledge_org_id),
-    CONSTRAINT designated_user_id_fkey FOREIGN KEY (user_id)
-        REFERENCES auth.users (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE,
-    CONSTRAINT designated_account_id_fkey FOREIGN KEY (account_id)
-        REFERENCES public.accounts (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE
+    weight float not null default 1 check (weight > 0),
+    created_at timestamptz not null default now(),
+    constraint designated_pkey primary key (user_id, account_id, pledge_org_id),
+    constraint designated_user_id_fkey foreign key (user_id)
+        references auth.users (id) match simple
+        on update no action
+        on delete cascade,
+    constraint designated_account_id_fkey foreign key (account_id)
+        references public.accounts (id) match simple
+        on update no action
+        on delete cascade
 );
 
-ALTER TABLE IF EXISTS public.designated
-    ENABLE ROW LEVEL SECURITY;
+alter table if exists public.designated
+    enable row level security;
 
-CREATE POLICY "Owner has all permissions"
-    ON public.designated
-    AS PERMISSIVE
-    FOR ALL
-    TO public
-    USING (auth.uid() = user_id);
+create policy "Owner has all permissions"
+    on public.designated
+    as permissive
+    for all
+    to public
+    using (auth.uid() = user_id);
 
-CREATE TABLE IF NOT EXISTS public.requests
+create table if not exists public.requests
 (
     id serial,
-    ngo_id text NOT NULL,
-    user_id uuid DEFAULT auth.uid(),
+    ngo_id text not null,
+    user_id uuid default auth.uid(),
     email email,
-    CONSTRAINT requests_pkey PRIMARY KEY (id),
-    CONSTRAINT requests_user_id_fkey FOREIGN KEY (user_id)
-        REFERENCES auth.users (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE
+    constraint requests_pkey primary key (id),
+    constraint requests_user_id_fkey foreign key (user_id)
+        references auth.users (id) match simple
+        on update no action
+        on delete cascade
 );
 
-ALTER TABLE IF EXISTS public.requests
-    ENABLE ROW LEVEL SECURITY;
+alter table if exists public.requests
+    enable row level security;
 
-CREATE POLICY "Anonymous users can create their own"
-ON public.requests
-    AS PERMISSIVE
-    FOR INSERT
-    TO anon
-    WITH CHECK (user_id is null and email is not null);
+create policy "Anonymous users can create their own"
+on public.requests
+    as permissive
+    for insert
+    to anon
+    with check (user_id is null and email is not null);
 
-CREATE POLICY "Authenticated users can create their own"
-ON public.requests
-    AS PERMISSIVE
-    FOR INSERT
-    TO authenticated
-    WITH CHECK (auth.uid() = user_id and email is null);
+create policy "Authenticated users can create their own"
+on public.requests
+    as permissive
+    for insert
+    to authenticated
+    with check (auth.uid() = user_id and email is null);
